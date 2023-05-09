3ef3aec3cdb1bef6cf72cc43db4c87b224f2a2fe
Abhishek Sharma <abhishekjps168@gmail.com>
2023-05-10 02:25:21 +0530

    Update todo.js (Sridhar)

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,5 +1,6 @@
 "use strict";
 const { Model } = require("sequelize");
+const { Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -10,15 +11,69 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
+
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
+
     static getTodos() {
       return this.findAll();
     }
-    static deleteAll() {
-      this.destroy({ where: {} });
+    static async overdue() {
+      // FILL IN HERE TO RETURN OVERDUE ITEMS
+
+      const today = new Date();
+      return Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: today,
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
     }
+
+    static async dueToday() {
+      // FILL IN HERE TO RETURN ITEMS DUE tODAY
+      const today = new Date();
+      return Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: today,
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static async dueLater() {
+      // FILL IN HERE TO RETURN ITEMS DUE LATER
+      const today = new Date();
+      return Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: today,
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static async markAsComplete(id) {
+      // FILL IN HERE TO MARK AN ITEM AS COMPLETE
+      return Todo.update(
+        { completed: true },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+    }
+
     markAsCompleted() {
       return this.update({ completed: true });
     }
