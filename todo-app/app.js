ca6d68b353e88375366a7caff320492f8fc0472d
Abhishek Sharma <abhishekjps168@gmail.com>
2023-05-10 02:27:21 +0530

    Update app.js (Sridhar)

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,66 +1,49 @@
-/* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
+const path = require("path");
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-const path = require("path");
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 
-const formattedDate = (d) => {
-  return d.toISOString().split("T")[0];
-};
+app.get("/", async (request, response) => {
+  //const allTodos = await Todo.getTodos();
+  const overduelist = await Todo.overdue();
+  const duetodaylist = await Todo.dueToday();
+  const duelaterlist = await Todo.dueLater();
 
-var dateToday = new Date();
-const today = formattedDate(dateToday);
-const yesterday = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() - 1))
-);
-const tomorrow = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() + 1))
-);
-
-app.get("/", async (req, res) => {
-  Todo.deleteAll();
-  Todo.addTodo({
-    title: "Submit homework",
-    dueDate: yesterday,
-    completed: false,
-  });
-  Todo.addTodo({ title: "Watch movie", dueDate: yesterday, completed: true });
-  Todo.addTodo({ title: "Buy book", dueDate: today, completed: false });
-  Todo.addTodo({ title: "File taxes", dueDate: tomorrow, completed: false });
-  Todo.addTodo({ title: "Pay AC bill", dueDate: tomorrow, completed: false });
-  console.log(Todo);
-  const allTodos = await Todo.getTodos();
-  if (req.accepts("html")) {
-    res.render("index", {
-      allTodos,
+  if (request.accepts("html")) {
+    response.render("index", {
+      overduelist,
+      duetodaylist,
+      duelaterlist,
     });
   } else {
-    res.json({ allTodos });
+    response.json({
+      overduelist,
+      duetodaylist,
+      duelaterlist,
+    });
   }
 });
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
-});
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
   // FILL IN YOUR CODE HERE
+
+  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
+  // Then, we have to respond with all Todos, like:
+  // response.send(todos)
   try {
-    const todos = await Todo.findAll();
-    return response.send(todos);
+    const todo = await Todo.findAll();
+    return response.json(todo);
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
-  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
-  // Then, we have to respond with all Todos, like:
-  // response.send(todos)
 });
 
 app.get("/todos/:id", async function (request, response) {
@@ -97,11 +80,21 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
   // FILL IN YOUR CODE HERE
-  const deleteTodo = await Todo.destroy({ where: { id: request.params.id } });
-  response.send(deleteTodo ? true : false);
+
   // First, we have to query our database to delete a Todo by ID.
   // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
   // response.send(true)
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    if (!todo) {
+      return response.json(false);
+    }
+    await todo.destroy();
+    return response.json(true);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 module.exports = app;
